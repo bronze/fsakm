3e9992a8614f37a1ffd8b2016e7b65b6020a1184
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-05-24 22:45:59 -0300

    checkpoint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '@/styles/global.css'
 
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
