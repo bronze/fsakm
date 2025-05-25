6b3b79ac0e970c62373b4861c8057d7cd54987ce
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-05-25 12:01:46 -0300

    checkpoint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
 import '@/styles/global.css'
 
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
-
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- theme.config.tsx
+++ theme.config.tsx
@@ -7,6 +7,9 @@ const config: DocsThemeConfig = {
   logo: <span>FSAKM NextJS</span>,
   logoLink: "/",
   chat: {},
+  sidebar: {
+    toggleButton: true,
+  },
   footer: {
     text: "FSAKM Test",
   },
