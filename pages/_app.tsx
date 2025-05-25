0bd41f1cab3e02622b485158c63e3e40cd532354
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-05-25 13:22:42 -0300

    checkpoint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
-import '@/styles/global.css'
+import {AppProps} from "next/app";
+import {Inter} from "next/font/google";
+import {ReactNode} from "react";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import "@/styles/global.css";
 
-{
-  /* <MyApp /> */
+const inter = Inter({subsets: ["latin"]});
+
+type Props = AppProps & {
+  Component: {getLayout?(page: ReactNode): ReactNode};
+};
+
+export default function App({Component, pageProps}: Props) {
+  const getLayout = Component.getLayout || ((page: ReactNode) => page);
+  return <div className={inter.className}>{getLayout(<Component {...pageProps} />)}</div>;
 }
---
+++ pages/blog/_meta.js
@@ -0,0 +1,10 @@
+export default {
+  index: {
+    type: "page",
+    title: "Blog Posts",
+    theme: {
+      typesetting: "article",
+      layout: "full"
+    }
+  }
+};
