f5da0ac725f350a1e37f6baba55847051677d2fa
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-05-25 22:50:33 -0300

    rounded

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -49,21 +49,21 @@ const footer = (
             href="https://www.instagram.com/kravmaga_mestrekobi/"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center justify-center w-10 h-10 hover:bg-gray-200 rounded-full group">
+            className="flex items-center justify-center w-10 h-10 hover:bg-gray-200 rounded group">
             <Instagram className="w-6 h-6 group-hover:text-fuchsia-500" />
           </a>
           <a
             href="https://www.facebook.com/kravmagamestrekobi/"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center justify-center w-10 h-10 hover:bg-gray-200 rounded-full group">
+            className="flex items-center justify-center w-10 h-10 hover:bg-gray-200 rounded group">
             <Facebook className="w-6 h-6 group-hover:text-blue-500" />
           </a>
           <a
             href="https://www.youtube.com/@kravmagamestrekobi/"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center justify-center w-10 h-10 hover:bg-gray-200 rounded-full group ">
+            className="flex items-center justify-center w-10 h-10 hover:bg-gray-200 rounded group ">
             <Youtube className="w-6 h-6 group-hover:text-red-500" />
           </a>
         </div>
