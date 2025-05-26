e765e90395065cadc6c53b7b61885d3be3399846
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-05-26 10:28:18 -0300

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,23 @@
-export {middleware} from "nextra/locales";
+// Ok so why use a different middleware file?
+// Because we want to handle the locale redirection separately from the i18n middleware.
+// Basically, i wanted the default language to always be Portuguese, even if my pc had a different lang.
+
+import {NextRequest, NextResponse} from "next/server";
+
+export function middleware(req: NextRequest) {
+  // Always use the defaultLocale from next.config.mjs
+  const url = req.nextUrl;
+  const {pathname, locale} = req.nextUrl;
+
+  // If the pathname already includes a locale, do nothingca
+  if (["/pt", "/en", "/es"].some(lng => pathname.startsWith(lng))) {
+    return NextResponse.next();
+  }
+
+  // Redirect to defaultLocale ('pt') if no locale in path
+  url.pathname = `/pt${pathname}`;
+  return NextResponse.redirect(url);
+}
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
