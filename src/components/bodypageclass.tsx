4442f0c5039be553a0c08f34ac194afbafb09800
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-05-27 14:36:44 -0300

    fonts

--- src/components/bodypageclass.tsx
+++ src/components/bodypageclass.tsx
@@ -1,22 +1,32 @@
 "use client";
 import {usePathname} from "next/navigation";
+import {Inter, Homemade_Apple} from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+const homemade = Homemade_Apple({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-homemade",
+  display: "swap",
+});
 
 export default function BodyWithPageClass({children}: {children: React.ReactNode}) {
-  //   const pathname = usePathname();
-  //   const pageClass = pathname === "/" ? "page-home" : "page-" + pathname.replace(/\//g, "-").replace(/^-/, "");
-  //   return <body className={pageClass}>{children}</body>;
-  // HOLY CRAP it got a lot more complicated
   const pathname = usePathname();
-  // Remove leading slash and split by "/"
   const parts = pathname.replace(/^\/+/, "").split("/");
+
   let pageClass;
   if (parts.length === 1 && parts[0]) {
-    // Only locale present, it's the home page for that locale
     pageClass = `page-${parts[0]}-home`;
   } else if (pathname === "/") {
     pageClass = "page-home";
   } else {
     pageClass = "page-" + pathname.replace(/\//g, "-").replace(/^-/, "");
   }
-  return <body className={pageClass}>{children}</body>;
+
+  return <body className={`${inter.variable} ${homemade.variable} ${pageClass}`}>{children}</body>;
 }
