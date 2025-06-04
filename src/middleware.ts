0a2b613cd01cc674d38c2eb1541e0d58b0119e7d
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-04 18:40:23 -0300

    Add header to locale rewrite

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,10 +6,15 @@ import {NextRequest, NextResponse} from "next/server"
 
 const SUPPORTED_LOCALES = ["pt", "en", "es"]
 const DEFAULT_LOCALE = "pt"
+const MIDDLEWARE_HEADER = "X-From-Middleware"
 
 export function middleware(req: NextRequest) {
   const url = req.nextUrl
   const {pathname} = url
+
+  if (req.headers.get(MIDDLEWARE_HEADER)) {
+    return NextResponse.next()
+  }
 
   // If the pathname already includes a locale, do nothing
   if (SUPPORTED_LOCALES.some(lng => pathname.startsWith(`/${lng}`))) {
@@ -25,7 +30,10 @@ export function middleware(req: NextRequest) {
     : DEFAULT_LOCALE
 
   url.pathname = `/${locale}${pathname}`
-  return NextResponse.redirect(url)
+  url.locale = locale
+  const res = NextResponse.rewrite(url)
+  res.headers.set(MIDDLEWARE_HEADER, "1")
+  return res
 }
 
 export const config = {
