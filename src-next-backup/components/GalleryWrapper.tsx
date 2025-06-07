08ef6dc92c77797c07647cf705d1757177d88a60
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-07 12:59:29 -0300

    astro stuff

--- src-next-backup/components/GalleryWrapper.tsx
+++ src-next-backup/components/GalleryWrapper.tsx
@@ -1,7 +1,7 @@
 import {getDictionary} from "@/src/app/_dictionaries/get-dictionary"
 import type {Locale} from "@/src/app/_dictionaries/i18n-config"
 
-import {Gallery} from "./gallery"
+import {Gallery} from "../../src/components/gallery"
 
 export default async function GalleryWrapper({
   children,
