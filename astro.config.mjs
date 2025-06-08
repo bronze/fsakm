7adc5c9fa6ea6a9f51c002b4765819d89e4f285b
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-08 13:09:22 -0300

    astro+shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  adapter: vercel()
+    vite: {
+        plugins: [tailwindcss()]
+    },
+
+    integrations: [react()]
 });
