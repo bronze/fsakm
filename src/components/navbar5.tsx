765e577af63288ad03e878c2cc25fa0f12b03ffa
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-09 17:28:54 -0300

    remove img

--- src/components/navbar5.tsx
+++ src/components/navbar5.tsx
@@ -62,11 +62,6 @@ const Navbar5 = () => {
             <div className="container mx-auto">
                 <nav className="flex items-center justify-between">
                     <a href="https://www.shadcnblocks.com" className="flex items-center gap-2">
-                        <img
-                            src="https://shadcnblocks.com/images/block/logos/shadcnblockscom-icon.svg"
-                            className="max-h-8"
-                            alt="Shadcn UI Navbar"
-                        />
                         <span className="text-lg font-semibold tracking-tighter">
                             Shadcnblocks.com
                         </span>
