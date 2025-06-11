0de5bcf4fa082f7b9ca10775bcd4ceb5e24f5ebd
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-11 17:41:15 -0300

    astro mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import {defineConfig} from 'astro/config';
 
 import icon from 'astro-icon';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
 
-    integrations: [react(), icon()],
+    integrations: [react(), icon(), mdx()],
 });
