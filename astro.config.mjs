7dc1bc5182ed4a57894b63ed417d233b0872c2c8
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-11 20:37:02 -0300

    checkpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import {imageService} from '@unpic/astro/service';
 import icon from 'astro-icon';
@@ -9,10 +10,8 @@ import {defineConfig} from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://fsakm-astro.cerlosbronze.com.br',
-
     // base: '/docs',
     base: process.env.NODE_ENV === 'production' ? '/' : '/',
-
     image: {
         service: imageService({
             // This can usually be auto-detected
@@ -22,10 +21,11 @@ export default defineConfig({
             layout: 'constrained',
         }),
     },
-
     vite: {
         plugins: [tailwindcss()],
     },
-
     integrations: [react(), icon(), mdx()],
+    adapter: vercel({
+        imageService: true,
+    }),
 });
