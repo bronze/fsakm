c2e2d3dcf5d4cb8f3bacde44680965847c1bfbe9
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-11 18:23:40 -0300

    images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
 // @ts-check
+import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import {defineConfig} from 'astro/config';
-
+import {imageService} from '@unpic/astro/service';
 import icon from 'astro-icon';
-
-import mdx from '@astrojs/mdx';
+import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    image: {
+        service: imageService({
+            // This can usually be auto-detected
+            fallbackService: 'astro', // astro or vercel?
+            placeholder: 'blurhash',
+            // This is the default
+            layout: 'constrained',
+        }),
+    },
     vite: {
         plugins: [tailwindcss()],
     },
-
     integrations: [react(), icon(), mdx()],
 });
