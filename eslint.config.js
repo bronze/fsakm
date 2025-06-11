bdc665e60cfc5c5ba0320ad8d7f571c2b5ca83d3
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-11 17:54:22 -0300

    astro eslint

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// https://github.com/ota-meshi/eslint-plugin-astro?tab=readme-ov-file
+
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+    // add more generic rule sets here, such as:
+    // js.configs.recommended,
+    ...eslintPluginAstro.configs.recommended,
+    {
+        rules: {
+            // override/add rules settings here, such as:
+            // "astro/no-set-html-directive": "error"
+        },
+    },
+];
