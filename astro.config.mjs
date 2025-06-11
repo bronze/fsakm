efe12b0fc57f484f835bfbe3c973df3f33aa3338
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-11 20:33:09 -0300

    checkpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import {imageService} from '@unpic/astro/service';
 import icon from 'astro-icon';
@@ -17,7 +16,7 @@ export default defineConfig({
     image: {
         service: imageService({
             // This can usually be auto-detected
-            fallbackService: 'vercel', // astro or vercel?
+            fallbackService: 'sharp', // astro or vercel?
             placeholder: 'blurhash',
             // This is the default
             layout: 'constrained',
@@ -29,7 +28,4 @@ export default defineConfig({
     },
 
     integrations: [react(), icon(), mdx()],
-    adapter: vercel({
-        imageService: true,
-    }),
 });
