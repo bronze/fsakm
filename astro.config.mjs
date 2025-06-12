ddf83207446230904f65a352143cf5244c927c42
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-12 14:10:00 -0300

    vercel test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
+import {imageService} from '@unpic/astro/service';
 import icon from 'astro-icon';
 import {defineConfig} from 'astro/config';
 
@@ -10,16 +12,23 @@ export default defineConfig({
     experimental: {
         responsiveImages: true,
     },
+
     image: {
         // Used for all Markdown images; not configurable per-image
         // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
         experimentalLayout: 'constrained',
+        service: imageService(),
     },
+
     // site: 'https://fsakm-astro.carlosbronze.com.br',
     // base: '/docs',
     // base: process.env.NODE_ENV === 'production' ? '/' : '/',
     vite: {
         plugins: [tailwindcss()],
     },
+
     integrations: [react(), icon(), mdx()],
+    adapter: vercel({
+        imageService: true,
+    }),
 });
