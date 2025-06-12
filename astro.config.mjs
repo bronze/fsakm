5a747c09cf9ccee023f5500ded34c2530e8b718c
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-11 21:25:59 -0300

    remove adapter and unpic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,17 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
-import {imageService} from '@unpic/astro/service';
 import icon from 'astro-icon';
 import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fsakm-astro.cerlosbronze.com.br',
+    // site: 'https://fsakm-astro.carlosbronze.com.br',
     // base: '/docs',
     // base: process.env.NODE_ENV === 'production' ? '/' : '/',
-    image: {
-        service: imageService({
-            // This can usually be auto-detected
-            // fallbackService: 'vercel', // astro or vercel?
-            placeholder: 'blurhash',
-            // This is the default
-            layout: 'constrained',
-        }),
-    },
     vite: {
         plugins: [tailwindcss()],
     },
     integrations: [react(), icon(), mdx()],
-    adapter: vercel({
-        imageService: false,
-    }),
 });
